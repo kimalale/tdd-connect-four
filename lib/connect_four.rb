cc2f4fbed33c42a24eebdd558d9384261dfcd25b
Kim Malale <kimalale@hotmail.com>
2023-09-06 00:32:36 +0200

    Moved driver code to main file

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -152,18 +152,3 @@ class ConnectFour
 
 
 end
-
-
-connect_four = ConnectFour.new
-player_input = nil
-
-until !connect_four.game_state do
-  connect_four.show_board
-  puts "Enter column to insert ball: "
-  player_input = gets.chomp.to_i
-  connect_four.place_ball(player_input)
-  connect_four.check_win
-
-end
-
-puts connect_four.message
